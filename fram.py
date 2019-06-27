9a0f8176c09d4eb9511930aaac3cd8c1af2fbb2e
Ankit Khandelwal <amtron521@gmail.com>
2019-06-27 18:15:46 -0400

    added reading and writing from fram

--- fram.py
+++ fram.py
@@ -3,19 +3,27 @@ import spidev
 
 WREN = 0x06
 WRITE = 0x02
+READ = 0x03
+SLEEP = 0xA9
 spi = spidev.SpiDev()
 
-#Only have SPI Bus 0 available on RPI
-bus = 0
+def start():
+    global spi
 
-#Device is the chip select pin. Set to 0 or 1, depending on connection
-device = 1
+    bus = 0 #Only have SPI Bus 0 available on RPI
+    device = 1 #Device is the chip select pin. Set to 0 or 1, depending on connection
+    spi.open(bus, device) #Opens connection with specific bus and device
+    spi.max_speed_hz = 31200000 #FRAM max is 40MHz and RPI only supports certain numbers
+    spi.mode=0
+    spi.cshigh = True
 
-#Opens connection with specific bus and device
-spi.open(bus, device)
+def write_to_fram(address, to_send):
+    spi.xfer([WREN])
+    spi.xfer([WRITE, address, to_send])
 
-spi.max_speed_hz = 31200000
-spi.mode=0
-
-spi.xfer([WREN])
-spi.xfer([WRITE, 0x01, 0x06])
+def read_from_fram(address, num_bytes):
+    spi.xfer([READ, address])
+    return spi.readBytes(num_bytes)
+        
+if __name__ == "__main__":
+    start()
